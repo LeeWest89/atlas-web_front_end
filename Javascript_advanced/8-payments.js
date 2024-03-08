184a71ef779e20960638b03a5c9aa287c0eae36d
LeeWest89 <lee.west@atlasschool.com>
2024-03-08 13:53:28 -0600

    processPayment and processOrder work as expected

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,17 @@
+//Runs to function that show the desired outcome for dealing with orders
+
+function processPayment(amount) {
+    console.log(`Collecting payment of ${amount}`);
+}
+
+function processOrder(orderId, amount){
+    console.log(`${orderId} is being processed`);
+    processPayment(amount);
+    console.log(`${orderId} has been fully processed`)
+}
+
+console.log(`Processing orders`);
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 10.99);
+console.log(`All the orders have been processed`);
